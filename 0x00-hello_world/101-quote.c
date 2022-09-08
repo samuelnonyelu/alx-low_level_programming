b653406103249c1a44fffd39dee9d43bc41e0713
Sam <samuelnonyelu2000@gmail.com>
2022-09-08 23:10:42 +0100

    a1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - prints the string without using the printf and puts
+ *
+ * Return: returns 1 to indicate error
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+
+	return (1);
+}

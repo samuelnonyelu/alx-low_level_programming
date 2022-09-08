a5b81a0593b67beaf31553f2b36b512b95aa6ed9
Sam <samuelnonyelu2000@gmail.com>
2022-09-08 20:18:33 +0100

    t5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - using printf, prints string to the console
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

d904164f49bd7da0ef6b762917fcb6273c38999e
Sam <samuelnonyelu2000@gmail.com>
2022-09-08 20:04:09 +0100

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - using puts, will print a string of characters to the output
+ *
+ * Return: returns 0
+ */
+
+int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
 	return (0);
 }

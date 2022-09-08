2b648cdda9e4260c96bf67a6585e03a94ebade6c
Sam <samuelnonyelu2000@gmail.com>
2022-09-08 23:13:35 +0100

    a1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }

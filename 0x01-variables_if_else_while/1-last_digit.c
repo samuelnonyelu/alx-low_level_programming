238eae3bd53d8332bfa28dc7bbf59dcc65c8fa72
Sam <samuelnonyelu2000@gmail.com>
2022-09-09 16:31:26 +0100

    qucksave

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function will print the last digits of random strings
+ *
+ * Return: 0 returned for success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	
+}

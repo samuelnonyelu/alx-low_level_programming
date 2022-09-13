585dd7c16e222b78e23e259d6e4302a253c3451c
Sam <samuelnonyelu2000@gmail.com>
2022-09-13 17:52:35 +0100

    t7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - self described
+ * @n: integer for which last digit is extracted
+ *
+ * Return: 0 for success
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		int a = -n;
+		int x = a % 10;
+
+		_putchar(x + '0');
+	}
+	else
+	{
+		int x = n % 10;
+
+		_putchar(x + '0');
+	}
+	return (0);
+}

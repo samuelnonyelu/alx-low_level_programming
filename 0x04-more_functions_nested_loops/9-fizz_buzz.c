e750f0bb44a3899fda3ededcd3cedef8d37b618d
Sam <samuelnonyelu2000@gmail.com>
2022-09-15 22:52:34 +0100

    t9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,26 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
+	int i = 1;
+	
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0  && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");

f682fd75d4e454879af9fb60a9e83a59d9fc1a6f
Sam <samuelnonyelu2000@gmail.com>
2022-09-15 21:00:40 +0100

    t7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal lines than end in \
+ * @n: number of times \ should be printed
+ *
+ * Return: 0 of success
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}

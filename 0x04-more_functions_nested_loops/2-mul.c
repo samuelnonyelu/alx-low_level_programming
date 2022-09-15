a65aaeaf2e3f4f659573ec8d174f20d137c7400e
Sam <samuelnonyelu2000@gmail.com>
2022-09-15 16:17:55 +0100

    multi tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function multiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 for success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - function will print numbers 0 - 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+	return;
+}

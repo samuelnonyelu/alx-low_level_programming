f23cbbfa248e659ff58a1b77abb643dc9f9378a8
Sam <samuelnonyelu2000@gmail.com>
2022-09-19 22:27:10 +0100

    t6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ * stlen - works like strlen function
+ * @p: pointer to string
+ *
+ * Return: c
+ */
+int stlen(char *p)
+{
+	int c = 0;
+
+	while (*(p + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+/**
  * puts2 - prints every other character of a string,
  * starting with the first character, followed by a new line
  * @str: string
@@ -8,10 +24,11 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; i < stlen(str); i += 2)
 	{
-		_putchar(*str);
-		str += 2;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

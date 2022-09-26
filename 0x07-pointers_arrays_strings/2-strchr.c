88271ca08027a4aa526bf7ddde12fb6d494f7659
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-09-26 21:15:10 +0100

    t2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,23 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
- * _strchr - man strchr
- * @s: string to check
- * @c: character to find
+ * _strchr - man strcpy
+ * @s: string
+ * @c: character to find in s
  *
- * Return: s
+ * Return: pointer to c
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+
+	while (*s)
 	{
-		if (*s == c)
-		{
+		if (c == *s)
 			return (s);
-		}
 		s++;
 	}
+	if (c == *s)
+		return (s);
 	return (NULL);
 }

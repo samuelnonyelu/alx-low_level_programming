f4b5bd6d2187fbcfb52f6bb56ff1aa1976364f57
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-09-28 18:40:37 +0100

    t4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - works similar to pow function
+ * @x: base
+ * @y: exponent
+ *
+ * Return: the x ^ y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (1);
+}

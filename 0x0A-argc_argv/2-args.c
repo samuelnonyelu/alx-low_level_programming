75b7fb54edea347d5862d3b8046c360f680ba2b7
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-09-30 16:12:54 +0100

    t1

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - check code
  * @argc - argument count as int which counts and stores no of command line arg
@@ -6,3 +7,16 @@
  *
  * Return: integer
  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+	return (0);
+}

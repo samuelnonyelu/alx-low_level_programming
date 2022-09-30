d7df09143f88297e744ef067948b7d83fcde282c
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-09-30 16:13:52 +0100

    t2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  * main - check code
- * @argc - argument count as int which counts and stores no of command line arg
- * @argv - argument array of character pointers listing all the arguments
+ * @argc: argument count as int which counts and stores no of command line arg
+ * @argv: argument array of character pointers listing all the arguments
  *
  * Return: integer
  */

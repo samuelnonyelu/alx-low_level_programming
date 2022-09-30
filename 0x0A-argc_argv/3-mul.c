d93d65969b7c57c6be532cb45ca0c8d56a27927e
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-09-30 16:34:14 +0100

    t3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,30 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - check code
- * @argc - argument count as int which counts and stores no of command line arg
- * @argv - argument array of character pointers listing all the arguments
+ * @argc: argument count as int which counts and stores no of command line arg
+ * @argv: argument array of character pointers listing all the arguments
  *
  * Return: integer
  */
+int main(int argc, char *argv[])
+{
+	int result;
+	
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc > 2)
+	{
+		int x = strtol(argv[1], NULL, 10);
+		int y = strtol(argv[2], NULL, 10);
+
+		result = x * y;
+
+		printf("%d\n", result);
+	}
+	return (0);
+}

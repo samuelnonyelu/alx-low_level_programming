41725177dbb2c3cfb616436849feeee545d89164
Samuel Nonyelu <77490158+samuelnonyelu@users.noreply.github.com>
2022-09-30 13:58:17 +0100

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,2 +1,14 @@
 #include "main.h"
-int _abs(int n);
+/**
+ *_abs - Check Holberton
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}

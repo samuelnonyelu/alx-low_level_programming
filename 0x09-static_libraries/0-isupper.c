3e78029a754f8c4dcb587ffb12baa9db92d4c84b
Samuel Nonyelu <77490158+samuelnonyelu@users.noreply.github.com>
2022-09-30 13:41:23 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,2 +1,22 @@
 #include "main.h"
-int _isupper(int c);
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
+}

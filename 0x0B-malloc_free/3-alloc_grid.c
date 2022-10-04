38732bc63e05a9251a8c1fa0596dc12986f8f52e
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-05 00:43:32 +0100

    t3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
-			free(array);
+			free(array[i]);
 			return (NULL);
 		}
 	}

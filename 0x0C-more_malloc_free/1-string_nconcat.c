8f6fa8ee2c1a918c0c4e68a420f1d2d6dfd1049b
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-06 21:06:26 +0100

    t1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,8 +20,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for(l1 = 0; s1[l1] != '\0'; l1++);
-	for(l2 = 0; s2[l2] != '\0'; l2++);
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		;
 
 	if (n >= l2)
 		n = l2;

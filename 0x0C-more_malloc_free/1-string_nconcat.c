0548747cb36580846efac616b2421435f037bc56
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-06 16:26:13 +0100

    half finised task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - function joins the s1 to the first n bytes of s2
+ * @s1: first string
+ * @s2: second string
+ * @n: n bytes of string 2
+ *
+ * Return: pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}

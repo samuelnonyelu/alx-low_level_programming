d472e952edd0777f2b2c4fb2745553923c5865ed
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-06 18:42:16 +0100

    t1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,56 +12,56 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *arrstr;
-    unsigned int l1, l2, l, i;
+	char *arrstr;
+	unsigned int l1, l2, l, i;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+	if (s1 == NULL)
+    	{
+        	s1 = "";
+    	}
+    	if (s2 == NULL)
+    	{
+        	s2 = "";
+    	}
 
-    l1 = strlen(s1);
-    l2 = strlen(s2);
-    l = l1 + l2 + 1;
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+	l = l1 + l2 + 1;
 
-    arrstr = malloc(l * sizeof(*arrstr));
+	arrstr = malloc(l * sizeof(*arrstr));
 
-    if (arrstr == NULL)
-    {
-        free(arrstr);
-        return NULL;
-    }
+	if (arrstr == NULL)
+    	{
+        	free(arrstr);
+        	return NULL;
+    	}
 
-    if (n >= l2)
-    {
-        for(i = 0; i < l1; i++)
-        {
-            arrstr[i] = s1[i];
-        }
-        for(i = 0; i < l2; i++)
-        {
-            arrstr[l1 + i] = s2[i];
-        }
-        arrstr[l - 1] = '\0';
+    	if (n >= l2)
+    	{
+        	for(i = 0; i < l1; i++)
+        	{
+            		arrstr[i] = s1[i];
+        	}
+        	for(i = 0; i < l2; i++)
+        	{
+            		arrstr[l1 + i] = s2[i];
+        	}
+        	arrstr[l - 1] = '\0';
 
-        return (arrstr);
-    }
-    else
-    {
-        for(i = 0; i < l1; i++)
-        {
-            arrstr[i] = s1[i];
-        }
-        for(i = 0; i < n && s2[i] != '\0'; i++)
-        {
-            arrstr[l1 + i] = s2[i];
-        }
-        arrstr[l - 1] = '\0';
+        	return (arrstr);
+    	}
+    	else
+    	{
+        	for(i = 0; i < l1; i++)
+        	{
+            		arrstr[i] = s1[i];
+        	}
+        	for(i = 0; i < n && s2[i] != '\0'; i++)
+        	{
+            		arrstr[l1 + i] = s2[i];
+        	}
+        	arrstr[l - 1] = '\0';
 
-        return (arrstr);
-    }
+        	return (arrstr);
+    	}
 }

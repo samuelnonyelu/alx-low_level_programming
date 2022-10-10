7f7f42cb8651675b13a84db0936890446a20b35e
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-10 19:06:34 +0100

    t4 corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,33 +17,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[name_len])
 		name_len++;
 	while (owner[owner_len])
 		owner_len++;
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 
 	name_cpy = malloc(name_len + 1);
 	if (name_cpy == NULL)
+	{
+		free(name_cpy);
 		return (NULL);
+	}
 	for (i = 0; name[i]; i++)
 		name_cpy[i] = name[i];
 	name_cpy[i] = '\0';
-
 	owner_cpy = malloc(owner_len + 1);
 	if (owner_cpy == NULL)
+	{
+		free(owner_cpy);
 		return (NULL);
+	}
 	for (i = 0; owner[i]; i++)
 		owner_cpy[i] = owner[i];
 	owner_cpy[i] = '\0';
-
 	new_dog->name = name_cpy;
 	new_dog->age = age;
 	new_dog->owner = owner_cpy;
-
 	return (new_dog);
 }

0eff860c75565afbfef221630c4f3a5cbf418028
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-10 13:57:14 +0100

    setup files and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - defines a new type dog with elements
+ * @name: string name
+ * @age: float age
+ * @owner: string owner name
+ *
+ * Description: same as above
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG*/

ae8d1b8fc11ccc82566f477b866092d1d2725bbe
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-10 17:48:51 +0100

    t4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,39 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *new_dog;
+	char *name_cpy, *owner_cpy;
+	int name_len = 0, owner_len = 0, i;
 
-	dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	while (name[name_len])
+		name_len++;
+	while (owner[owner_len])
+		owner_len++;
 
-	return (dog);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	name_cpy = malloc(name_len + 1);
+	if (name_cpy == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		name_cpy[i] = name[i];
+	name_cpy[i] = '\0';
+
+	owner_cpy = malloc(owner_len + 1);
+	if (owner_cpy == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		owner_cpy[i] = owner[i];
+	owner_cpy[i] = '\0';
+
+	new_dog->name = name_cpy;
+	new_dog->age = age;
+	new_dog->owner = owner_cpy;
+
+	return (new_dog);
 }

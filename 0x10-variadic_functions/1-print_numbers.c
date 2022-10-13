dff9da17e082c41f9e57cbfc5f60fd59e2ce9476
Samuel Nonyelu <samuelnonyelu2000@Gmail.com>
2022-10-13 10:41:44 +0100

    t1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers spaced by a user defined separator and "\n"
+ * @separator: string character to space the numbers like , . / etc
+ * @n: number of integers
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int x;
+	va_list nums;
+
+	if (separator == NULL)
+		separator = "";
+
+	if (n == 0)
+		return;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(nums, int);
+		printf("%d", x);
+
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
